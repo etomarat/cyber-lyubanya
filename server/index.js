6927d1bb39bad7d621de8c93ca46aa074025ab69
etomarat <etomarat@gmail.com>
2016-05-22 21:02:50 +0300

    fix port

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ let socketio = require('socket.io')(server);
 let routes = require('./routes')(app, socketio);
 let connections = require('./connections');
 
-let port = 80;
+let port = 8010;
 
 app.use(express.static('client/assets'));
 
